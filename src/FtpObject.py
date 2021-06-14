7538894013aa60ff37f7d99101ec071086c322c4
valouu_ <valentingoulier@gmail.com>
2021-06-14 15:36:31 +0200

    🗃 Adding directory tree

--- src/FtpObject.py
+++ src/FtpObject.py
@@ -1,11 +1,23 @@
 from ftplib import FTP
+import ftplib
 import json, datetime, shutil, os
 
 class FtpObject():
 
     def __init__(self):
-        #   Chargement de la configuration
+        #   Load configuration
         with open("config.json", "r") as config:
             self.config = json.load(config)
-        #   Connexion au server FTP
+        #   FTP connect
         self.ftp = FTP(self.config["ftp_server"], self.config["ftp_username"], self.config["ftp_password"])
+        self.root = "%s/%s/" % (self.ftp.pwd(), self.config["name_backup"])
+        self.directory = []
+
+    def directory_tree(self, dir):
+        try:
+            for directory in self.ftp.nlst(dir):
+                if directory.find(".") == -1: # Exclude files
+                    self.directory.append(directory[len(self.config["name_backup"])+2:])
+                    self.test(directory)
+        except ftplib.error_perm as e:
+            print(e)
