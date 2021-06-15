e3af2a0b680b3bf60911fed3b57dc06002854f24
valouu_ <valentingoulier@gmail.com>
2021-06-15 21:14:10 +0200

    🖥 Loading local files and making a tree directory

--- src/LocalObject.py
+++ src/LocalObject.py
@@ -10,3 +10,14 @@ class LocalObject():
         self.directory = []
         self.files = []
         
+        self.__loading_files__()
+
+    def __loading_files__(self):
+
+        for (directory, sousRepertoires, fichiers) in os.walk(self.config["dir_backup"]):
+            if directory.find(".") == -1: # Exlude hidden folders
+                # Save directory
+                self.directory.append(directory[len(self.config["dir_backup"])+1:])
+                # Save file in FileObject
+                for fichier in fichiers:
+                    self.files.append(FileObject(directory, fichier))
