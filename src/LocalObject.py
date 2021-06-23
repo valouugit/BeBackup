1dfb7347e27570b8e28e91e8b5132b66fbf5b863
valouu_ <valentingoulier@gmail.com>
2021-06-23 11:15:33 +0200

    🖥 Get files list / Fonction rename

--- src/LocalObject.py
+++ src/LocalObject.py
@@ -10,15 +10,14 @@ class LocalObject():
         self.directory = []
         self.files = []
         
-        self.__loading_files__()
+        self.tree()
 
-    def __loading_files__(self):
-
+    def tree(self):
         for (directory, sousRepertoires, fichiers) in os.walk(self.config["dir_backup"]):
             if directory.find(".") == -1: # Exlude hidden folders
                 # Save directory
                 if len(directory) != len(self.config["dir_backup"]): # Exclude root dir
                     self.directory.append(directory[len(self.config["dir_backup"])+1:])
-                # Save file in FileObject
+                # Save file
                 for fichier in fichiers:
-                    self.files.append(FileObject(directory, fichier))
+                    self.files.append("%s\\%s" % (directory[len(self.config["dir_backup"]):], fichier))
