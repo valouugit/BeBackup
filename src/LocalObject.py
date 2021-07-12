cac1ba077e9c2fa144de02984eeddac92baaf849
valouugit <valentingoulier@gmail.com>
2021-07-12 13:44:24 +0200

    🖥 Adding a dynamic configuration

--- src/LocalObject.py
+++ src/LocalObject.py
@@ -2,29 +2,27 @@ import json, os
 
 class LocalObject():
     
-    def __init__(self):
-        #   Load configuration
-        with open("config.json", "r") as config:
-            self.config = json.load(config)
+    def __init__(self, ftp_config):
+        self.ftp_config = ftp_config
         self.directory = []
         self.files = []
         
         self.tree()
 
     def tree(self):
-        for (directory, sousRepertoires, fichiers) in os.walk(self.config["dir_backup"]):
+        for (directory, sousRepertoires, fichiers) in os.walk(self.ftp_config.backup_dir):
             if directory.find(".") == -1: # Exlude hidden folders
                 # Save directory
-                if len(directory) != len(self.config["dir_backup"]): # Exclude root dir
-                    self.directory.append(directory[len(self.config["dir_backup"])+1:])
+                if len(directory) != len(self.ftp_config.backup_dir): # Exclude root dir
+                    self.directory.append(directory[len(self.ftp_config.backup_dir)+1:])
                 # Save file
                 for fichier in fichiers:
                     if fichier.find(".") != -1: # Exclude file without extension
-                        file = "%s\\%s" % (directory[len(self.config["dir_backup"]):], fichier)
+                        file = "%s\\%s" % (directory[len(self.ftp_config.backup_dir):], fichier)
                         self.files.append(file.lstrip("\\"))
 
     def get_time(self, dir, file):
-        return os.path.getmtime("%s/%s/%s" % (self.config["dir_backup"], dir, file))
+        return os.path.getmtime("%s/%s/%s" % (self.ftp_config.backup_dir, dir, file))
     
     def get_timestamp(self):
         return os.path.getmtime("timestamp.ts")
