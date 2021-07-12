25a8772a1216c061c844802590d86d67526c6b40
valouugit <valentingoulier@gmail.com>
2021-07-12 13:44:44 +0200

    🗃 Adding a dynamic configuration

--- src/FtpObject.py
+++ src/FtpObject.py
@@ -5,14 +5,12 @@ import json, time
 
 class FtpObject():
 
-    def __init__(self, log = True):
+    def __init__(self, log = True, ftp_config = None):
         self.log = log
-        #   Load configuration
-        with open("config.json", "r") as config:
-            self.config = json.load(config)
+        self.ftp_config = ftp_config
         #   FTP connect
-        self.ftp = FTP(self.config["ftp_server"], self.config["ftp_username"], self.config["ftp_password"])
-        self.root = "%s/%s/" % (self.ftp.pwd(), self.config["name_backup"])
+        self.ftp = FTP(self.ftp_config.hostname, self.ftp_config.username, self.ftp_config.password)
+        self.root = "%s/%s/" % (self.ftp.pwd(), self.ftp_config.backup_name)
         self.directory = []
         self.files = []
         self.temp_directory = []
@@ -22,12 +20,13 @@ class FtpObject():
         self.tree(self.root)
 
     def create_dir_backup(self):
-        if not self.config["name_backup"] in self.ftp.nlst():
-            self.ftp.mkd(self.config["name_backup"])
+        if not self.ftp_config.backup_name in self.ftp.nlst():
+            self.ftp.mkd(self.ftp_config.backup_name)
 
     def tree(self, dir, temp=False):
         try:
             for item in self.ftp.nlst(dir):
+                print(item)
                 if "." in item: # Exclude files
                     if temp:
                         self.temp_files.append(item.lstrip(self.root))
@@ -44,13 +43,13 @@ class FtpObject():
             print(e)
 
     def dir_push(self, parent, dir):
-        self.ftp.cwd("/%s/%s" % (self.config["name_backup"], parent)) # Moove to parent dir
+        self.ftp.cwd("/%s/%s" % (self.ftp_config.backup_name, parent)) # Moove to parent dir
         self.ftp.mkd(dir) # Create directory
         if self.log: print("Sending directory '%s%s'" % (parent, dir))
 
     def dir_del(self, parent, dir_del):
         try:
-            self.ftp.cwd("/%s/%s" % (self.config["name_backup"], parent)) # Moove to parent dir
+            self.ftp.cwd("/%s/%s" % (self.ftp_config.backup_name, parent)) # Moove to parent dir
             self.ftp.rmd(dir_del) # Delete directory
             if self.log: print("Deleting directory '%s%s'" % (parent, dir_del))
         except ftplib.error_perm as e:
@@ -58,7 +57,7 @@ class FtpObject():
 
     def file_push(self, dir=None, file=None, timestamp=False):
         if not timestamp:
-            with open("%s/%s/%s" % (self.config["dir_backup"], dir, file), "rb") as file_to_push:
+            with open("%s/%s/%s" % (self.ftp_config.backup_dir, dir, file), "rb") as file_to_push:
                 dir = c.dir_windows_to_ftp(dir)
                 self.ftp.cwd("/%s/%s" % (self.root, dir))
                 self.ftp.storbinary('STOR ' + file, file_to_push)
