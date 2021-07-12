c5afd5135a822b43eab6ef1723df5f80c6b283ed
valouugit <valentingoulier@gmail.com>
2021-07-12 15:39:58 +0200

    🌌 Simple application with dynamic configuration

--- bebackup.py
+++ bebackup.py
@@ -1,4 +1,16 @@
 from src.Sync import Sync
+from src.FTPConfig import FTPConfig
 
-sync = Sync()
+print("""
+  ██████╗ ███████╗██████╗  █████╗  ██████╗██╗  ██╗██╗   ██╗██████╗ 
+  ██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██║   ██║██╔══██╗
+  ██████╔╝█████╗  ██████╔╝███████║██║     █████╔╝ ██║   ██║██████╔╝
+  ██╔══██╗██╔══╝  ██╔══██╗██╔══██║██║     ██╔═██╗ ██║   ██║██╔═══╝ 
+  ██████╔╝███████╗██████╔╝██║  ██║╚██████╗██║  ██╗╚██████╔╝██║     
+  ╚═════╝ ╚══════╝╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝""")
+
+
+ftp_config = FTPConfig("hostname", "username", "password")
+
+sync = Sync(ftp_config=ftp_config, backup_dir="C:\\Users\\user\\Documents\\", backup_name="BeBackup")
 sync.sync()
